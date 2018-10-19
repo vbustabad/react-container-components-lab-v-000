55b9b1c8e430f4109844a3e921b4259021ba2ee4
Veronica Bustabad <vbustabad@gmail.com>
2018-10-19 19:05:47 +0000

    automatically backed up by learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,29 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.nytimes.com/svc/movies/v2/reviews/all.json?')
+    .then(response => response.json())
+    .then(reviews => this.setState({reviews}));
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        {reviews}
+      </div>
+    );
+  }
+}
+
+export default LatestMovieReviewsContainer;
